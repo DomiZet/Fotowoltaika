056bced6da0f10822c106206d7812d139906afbc
DomiZet <dominika.zwolenik@gmail.com>
2020-04-23 22:10:12 +0200

    Finished project's gallery.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,40 @@
 
 import React from 'react';
-import './Styles.css';
+import './Styles.scss';
 
 
 function Footer() {
   return (
-    <div className="App">
-      Stopka
+    <div className="footerContainer">
+      
+      <div className="footerText">
+        <h1>Solar</h1>
+        <p>
+        Sed ut perspiciatis unde omnis iste natus error, totam rem aperiam, eaque veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+        </p>
+      </div>
+
+      <div className="footerYork">
+      <h1>New York</h1>
+      <p>709 Honey creek Dr.</p>
+      <p> New York</p>
+      <p> NY 10028</p>
+      <div></div>
+      <p> 1-88-299-2000</p>
+      <p> youremail@company.com</p>
+      </div>
+
+      <div className="footerLondon">
+        <h1>London</h1>
+        <p>4821 Willow Greene Drive</p>
+        <p> Montgomery</p>
+        <p> AL 36109</p>
+        <div></div>
+        <p> 1-88-299-2000</p>
+        <p> youremail@company.com</p>
+      </div>
+
+
     </div>
   );
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,15 +1,83 @@
 
 import React from 'react';
-import './Styles.css';
+import './Styles.scss';
 
 
-function Projects() {
-  return (
-    <div className="App">
-      Projekty
-    </div>
-  );
+class Projects extends React.Component {
+  state={
+    numberImgToDisplay: 1,
+    // imgsToDisplay: [1]
+    imgsToDisplay: [1, 2, 3, 4]
+    
+  }
+
+  render() {
+
+    return (
+      
+      <div className="projectContainer">
+          
+        <h1>Our Projects</h1>
+
+        
+        <div className="projectWrap">
+        <div>
+
+          {
+            this.state.imgsToDisplay.map((img) => {
+              return (
+                <img width={300} src={require(`../../assets/images/projects/proj${img}.png`)} />
+              )
+              
+            }
+
+            )
+          }
+        </div>
+
+
+    
+        </div>
+
+        <button onClick={this.addMoreImg}>
+          Load More
+        </button>
+
+
+      </div>
+      
+    );
+
+
+  }
+
+
+  addMoreImg = () => {
+    
+    // this.setState ({
+    //    numberImgToDisplay: this.state.numberImgToDisplay + 1,
+    //   })
+
+    
+    // this.state.imgsToDisplay.push(this.state.numberImgToDisplay)
+    
+    
+    
+    this.state.imgsToDisplay.push(5, 6, 7, 8)
+
+    this.setState({})
+ 
+    // this.setState.imgsToDisplay.push(5)
+
+
+  }
+
+
+
 }
 
 
 export default Projects;
+
+
+
